51619253a9fcbbbeae04dee389f9fde565e86ef3
rgwdteens <rgwdteens@gmail.com>
2026-07-22 15:22:40 -0400

    Updated with new backend URL from Render requireaccess.jsx

--- PlumbingProjectApp/components/requireaccess.jsx
+++ PlumbingProjectApp/components/requireaccess.jsx
@@ -27,7 +27,7 @@ export function RequireAccess({
       setLoadingRole(true);
 
       const idToken = await firebaseUser.getIdToken(true);
-      const res = await axios.post("https://bibliomaniacs.onrender.com/get_user_role", { idToken });
+      const res = await axios.post("https://bibliomaniacs-ytnd.onrender.com/get_user_role", { idToken });
 
       const roleValue = typeof res.data === "string" ? res.data : res.data.role;
       setRole(roleValue ?? "user");
